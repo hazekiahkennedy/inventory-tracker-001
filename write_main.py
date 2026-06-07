1777f1b0e880d6888fdd48807121e95fc07e2cb1
hazekiahkennedy <hazekiah.kennedy@gmail.com>
2026-06-06 21:43:08 -0400

    Fix README filename and clean up extra files

--- write_main.py
+++
@@ -1,5 +0,0 @@
-
-import os
-path = r"C:\Users\hazek\OneDrive\Documents\CLOUD LAPS\inventory-tracker-001"
-with open(os.path.join(path, "main.tf"), "w") as f:
-    f.write(open(os.path.join(path, "main.tf")).read())
